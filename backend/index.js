e7df2b289cf0f53b758a4aded25026e7b2f163d2
Akash Sharma <akash1991@gmail.com>
2022-04-09 00:29:16 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { url } = require('inspector');
+const { v4: uuidv4 } = require('uuid');
+// const { url } = require('inspector');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const path = require('path');
 const port = 8000;
 
@@ -7,8 +9,44 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 app.use(express.static('assets'));
+
+
+const uri = "mongodb+srv://root:toor@cluster0.iafal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    if (err) {
+        console.log('Unable to connect to Mongo.');
+        process.exit(1);
+    } else {
+        console.log('connected to db 3000...');
+    }
+});
+const tasksCollection = client.db('test').collection('tasks');
+
+// const tasksCollection = db.collection('tasks');
+
+app.post('/create-task', function(req, res){
+    // toDoList.push({
+    //     task: req.body.task,
+    //     date: req.body.date,
+    //     option: req.body.option
+    // });
+    const id = uuidv4();
+    req.body = {
+        ...req.body,
+        taskId: id
+    };
+    tasksCollection.insertOne(req.body).then(result => {
+        console.log(result);
+    }).catch(error => console.error(error))
+      
+    // toDoList.push(req.body);
+    return res.redirect('/');
+    // return res.redirect('back');
+  });
 
 var toDoList = [
     // {
@@ -24,11 +62,15 @@ var toDoList = [
 ]
 
 app.get('/', function(req, res){
-
-    return res.render('home', {
-        title: "My To-Do List",
-        todo_List: toDoList
-    });
+    tasksCollection.find().toArray()
+        .then(results => {
+        console.log(results);
+        return res.render('home', {
+            title: "My To-Do List",
+            todo_List: results
+        });
+    })
+    .catch(error => console.error(error))
 })
 
 // app.get('/delete-task/', function(req, res){
@@ -38,14 +80,16 @@ app.get('/', function(req, res){
 // })
 var complete = ["finish jquery"]
 
-app.get('/removetask', function(req, res){
-    var deleteTask = req.body.check;
-    let taskIndex = toDoList.findIndex(task => task.deleteTask == deleteTask);
-    
-    if(taskIndex != -1){
-        toDoList.splice(taskIndex, 1);
+app.post('/removetask', function(req, res){
+    console.log(req.body);
+    var deleteTask = req.body.checkbox;
+    if(deleteTask === "on") {
+        toDoList = [];
     }
-
+    // let taskIndex = toDoList.findIndex(task => task.deleteTask == deleteTask);
+    // if(taskIndex != -1){
+    //     toDoList.splice(taskIndex, 1);
+    // }
     return res.redirect('back');
 })
 
@@ -59,18 +103,6 @@ app.get('/removetask', function(req, res){
 
 //     return res.redirect('back');
 // })
-
-app.post('/create-task', function(req, res){
-    // toDoList.push({
-    //     task: req.body.task,
-    //     date: req.body.date,
-    //     option: req.body.option
-    // });
-
-    toDoList.push(req.body);
-    return res.redirect('/');
-    // return res.redirect('back');
-})
 
 app.listen(port, function(err){
     if(err){
